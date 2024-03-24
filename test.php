d5355357e5269d70830d9c448295bdcb4089a957
nomura <nomjic@gmail.com>
2024-03-24 09:26:04 +0900

    Create test.php

---
+++ test.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="ja">
+
+  <head>
+    <meta charset="utf-8">
+    <title>トップページ</title>
+  </head>
+
+  <body>
+<p>php使えるかのテスト</p>
+<?php echo '<p>Hello World</p>'; ?>
+
+  </body>
+
+</html>
