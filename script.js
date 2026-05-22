41ad459651b8adba1cd1f6f286bd5a5ebe77420d
nomura <nomjic@gmail.com>
2026-05-23 01:35:10 +0900

    Implement URL parameter checks for redirection
    Added URL parameter checks for poster_qr and mobiletest to handle redirection and device type setting.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // ブラウザとOSを判定
 var result = UAParser();
-if(new URLSearchParams(window.location.search).has('mobiletest')){
+
+const params = new URLSearchParams(window.location.search);
+
+// poster_qr が含まれていたら、ポスターセッションFrameへリダイレクト
+if (params.has('poster_qr')) {
+  window.location.href = 'https://miro.com/app/board/uXjVGm8lqXQ=/?moveToWidget=3458764588434753095&cot=14';
+}
+
+// mobiletest が含まれていたら強制モバイル判定
+if (params.has('mobiletest')) {
   result.device.type = 'mobile';
 }
 
@@ -8,9 +17,12 @@ function setOnlineStage(){
   if(result.device.type === 'mobile') {
     // iPhoneまたはAndroidならモーダル出す
     $('body').addClass('is-mobile');
+
     // iframeは消す（背景をmiro会場のキャプチャにする）
     $('#miroFrameWrapper iframe').remove();
+
   } else {
+
     // Mac Safariだとiframeでクロスドメインエラー出るのでリダイレクト
     // iframe使用で何か不都合ある場合は、ここのif分岐を取り払って、無条件にリダイレクトさせればOK
     if(result.browser.name === 'Safari') {
